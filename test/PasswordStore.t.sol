f571f70e04631122d3b92ccce32f919f90dd74c1
PranavRJoshi <pranavrjoshi1@gmail.com>
2024-04-20 19:49:50 +0545

    add audit test

--- test/PasswordStore.t.sol
+++ test/PasswordStore.t.sol
@@ -30,4 +30,18 @@ contract PasswordStoreTest is Test {
         vm.expectRevert(PasswordStore.PasswordStore__NotOwner.selector);
         passwordStore.getPassword();
     }
+
+    // @audit-test Function to test whether anyone can set the password or not
+    // @notice Here, we assume that someone having an address of random_address can interact with an existing contract, 
+    // call the setPassword function and change the password even though random_address is not the owner
+    function test_anyone_can_set_password(address random_address) public {
+        vm.assume(random_address != owner); // assuming that the random_address is any address apart from the owner's address
+        vm.prank(random_address); // interacting with the contract using random_address
+        string memory expected_password = "myNewPassword";
+        passwordStore.setPassword(expected_password); // calling the setPassword method by the random_address
+
+        vm.prank(owner); // interacting the contract with the owner's address
+        string memory actual_password = passwordStore.getPassword(); // calling the getPassword method using the owner's address
+        assertEq(actual_password, expected_password); // assert the two variables to be equal, else throw an error
+    }
 }
